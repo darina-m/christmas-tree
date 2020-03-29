4ae16da410f8e3d2b51a567a98cc9a00f2b45073
unknown <you@example.com>
2020-03-30 00:21:07 +0600

    Added new components Logo, Nav

--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Logo from "../Logo/Logo";
+import Nav from "./Nav/Nav";
 import classes from "./Toolbar.module.css"
 
 export default () => (
-<div className={classes.Toolbar}>Toolbar
-Toolbar
+<div className={classes.Toolbar}>
+<Logo/>
+<Nav/>
 </div>
 );
