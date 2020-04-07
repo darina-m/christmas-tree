be1001b6f8d85587557703528bec124726533428
unknown <you@example.com>
2020-04-07 22:59:00 +0600

    Tried to add image

--- src/containers/ChristmasTree/ChristmasTree.js
+++ src/containers/ChristmasTree/ChristmasTree.js
@@ -4,7 +4,7 @@ import classes from "./ChristmasTree.module.css";
 
 export default ({ children }) => (
     <div className={classes.ChristmasTree}>
-       <ToysKit/>
+       <ToysKit image="https://thumbs.dreamstime.com/z/%D1%80%D0%B5%D0%B0%D0%BB%D0%B8%D1%81%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D0%B0%D1%8F-%D1%80%D0%BE%D0%B6%D0%B4%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-%D0%B5%D0%BB%D0%BA%D0%B0-%D1%82%D1%80%D0%B0%D0%B4%D0%B8%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB-104381843.jpg"/>
        ToysControls
     </div>
 );
