3e39fefadc5f2eb52f66cb086987914c8cb94ee0
darina-m <prosto.chelovek333@gmail.com>
2020-04-26 13:00:36 +0600

    Add Logo and Navogation to Drawer component

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Logo from "../../UI/Logo/Logo";
+import NavigationItem from "../../Layout/Toolbar/Navigation/Navigation";
 import classes from "./Drawer.module.css";
 
 export default () => (
-<div className={classes.Drawer}>
-    SideDrawer
-    </div>
+  <div className={classes.Drawer}>
+    <Logo />
+    <NavigationItem/>
+  </div>
 );
