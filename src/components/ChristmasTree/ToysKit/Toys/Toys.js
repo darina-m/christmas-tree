7d7da35d11ccb9444a217cb2e7a67d19c7503a22
darina-m <prosto.chelovek333@gmail.com>
2020-04-27 11:38:13 +0600

    change color in price , items, orderButton

--- src/components/ChristmasTree/ToysKit/Toys/Toys.js
+++ src/components/ChristmasTree/ToysKit/Toys/Toys.js
@@ -4,18 +4,18 @@ import classes from "./Toys.module.css";
 export default ({ type }) => {
   const toysClasses = [classes.Toys, classes[type]];
 
- /* let stylePos = null;
+ /*let stylePos = null;
     const getPosition = (ir) => {
       const pd = 210;
       const pr = pd / 2;
   
-      const ix = Math.round(Math.random() * pd);
-      const iy = Math.round(Math.random() * pd);
+      const ix = Math.round(Math.random(1) * pd);
+      const iy = Math.round(Math.random(2) * pd - pr);
   
       const distance =
         Math.sqrt(Math.pow(ix - pr, 2) + Math.pow(iy - pr, 2)) + ir;
         
-   /* return distance < pr ? { x: ix + ir, y: iy + ir } : getPosition(ir);
+   return distance < pr ? { x: ix - ir, y: iy - ir } : getPosition(ir);
   };*/
 
   switch (type) {
@@ -40,18 +40,17 @@ export default ({ type }) => {
       break;
   }
 
- // const position = getPosition(50 / 2);
+  /*const position = getPosition(50 / 2);
 
- /* stylePos = {
+ stylePos = {
     position: "absolute",
     top: position.y + "px",
     left: position.x + "px",
      width: 30 + "px",
       height: 30 + "px"
-  };
-  */
-
+  };*/
+  
 //style={stylePos}
-  return <div className={toysClasses.join(" ")}></div>;
+  return <div  className={toysClasses.join(" ")}></div>;
   
 };
