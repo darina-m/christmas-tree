4b8ad922e41d12d09a63ee84333e0d98fb8c93a3
darina-m <prosto.chelovek333@gmail.com>
2020-05-03 15:07:03 +0600

    changes #3

--- src/components/ChristmasTree/ToysKit/Toys/Toys.js
+++ src/components/ChristmasTree/ToysKit/Toys/Toys.js
@@ -3,25 +3,23 @@ import classes from "./Toys.module.css";
 
 export default memo(({ type }) => {
   const toysClasses = [classes.Toys, classes[type]];
-  
+
+  const pd = 40;
+  const th = 450;
+  const tw = 212;
+
   let stylePos = null;
   const getPosition = (ir) => {
-    const pd = 40;
-    const th = 450;
-    const tw = 212;
-  
-    const ix = Math.floor(Math.random() * tw, 0);
-    const iy = Math.floor(Math.random() * th, 0);
+
+
+    const ix = Math.floor(Math.random() * tw, 1);
+    const iy = Math.floor(Math.random() * th, 5);
 
     const distance =
-    Math.sqrt(Math.pow(ix - th, 2) + Math.pow(iy - th, 2)) + ir;
+      Math.sqrt(Math.pow(ix - th, 2) + Math.pow(iy - th, 2)) + ir;
 
-  return distance < th ? { x: ix - ir, y: iy - ir } : getPosition(ir);
-
-
+    return distance < th ? { x: ix - ir, y: iy - ir } : getPosition(ir);
   };
-
-
 
   switch (type) {
     case "yellowBall":
@@ -44,7 +42,7 @@ export default memo(({ type }) => {
       toysClasses.push(classes.blueBall);
       break;
   }
-  
+
   const position = getPosition(110 / 6);
 
   stylePos = {
