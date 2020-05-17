665946b101ea92bef848658fa09e676fa114e1cd
darina-m <prosto.chelovek333@gmail.com>
2020-05-17 13:16:18 +0600

    Add routs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import "./App.css";
 import Layout from "./containers/Layout/Layout.js";
 import ChristmasTree from "./containers/ChristmasTree/ChristmasTree";
 import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 export default () => {
   return (
     <div className="App">
       <Layout>
-        <ChristmasTree />
-        <Checkout/>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/builder" />
+          </Route>
+          <Route path="/builder">
+            <ChristmasTree />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
