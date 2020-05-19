eaa801cff001dc217d4f86d3f56872834459144c
darina-m <prosto.chelovek333@gmail.com>
2020-05-19 15:04:44 +0600

    Change progect in firebase

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create();
-instance.defaults.baseURL = "https://christmas-tree-424ce.firebaseio.com";
+instance.defaults.baseURL = "https://christmas-tree-97266.firebaseio.com";
 
 export default instance;
