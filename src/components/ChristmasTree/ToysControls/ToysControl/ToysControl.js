78c2389b0a177c65de4de4b1cd5ceff419b30fd7
darina-m <prosto.chelovek333@gmail.com>
2020-05-25 00:47:34 +0600

    Use dispatcher

--- src/components/ChristmasTree/ToysControls/ToysControl/ToysControl.js
+++ src/components/ChristmasTree/ToysControls/ToysControl/ToysControl.js
@@ -1,18 +1,25 @@
 import React from "react";
 import classes from "./ToysControl.module.css";
+import { useDispatch } from "react-redux";
+import { ADD_TOY, REMOVE_TOY } from "../../../../store/actions";
 
 export default ({ control, removeToy, addToy, disabled }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className={classes.ToysControl}>
       <button
         className={classes.less}
-        onClick={() => removeToy(control.type)}
+        onClick={() => dispatch({ type: REMOVE_TOY })}
         disabled={disabled}
       >
         -
       </button>
       <span className={classes.label}>{control.label}</span>
-      <button className={classes.more} onClick={() => addToy(control.type)}>
+      <button
+        className={classes.more}
+        onClick={() => dispatch({ type: ADD_TOY })}
+      >
         +
       </button>
     </div>
