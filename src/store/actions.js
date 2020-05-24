809352a520b9eab6f0a39a6b4db6f91c20d927b7
darina-m <prosto.chelovek333@gmail.com>
2020-05-25 00:28:36 +0600

    Add action types

---
+++ src/store/actions.js
@@ -0,0 +1,2 @@
+export const ADD_TOY = "ADD_TOY";
+export const REMOVE_TOY = "REMOVE_TOY";
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,19 @@
+import * as actions from "./actions";
+
 const initialState = {
   toys: null,
   price: 100,
 };
 
 export default (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case actions.ADD_TOY:
+      return state;
+
+    case actions.REMOVE_TOY:
+      return state;
+
+    default:
+      return state;
+  }
 };
