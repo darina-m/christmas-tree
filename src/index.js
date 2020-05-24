2b0f56083c8f4f34308b74e698be4b0cb27ae71e
darina-m <prosto.chelovek333@gmail.com>
2020-05-25 00:16:06 +0600

    Add store and reducer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import reducer from "./store/reducer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
