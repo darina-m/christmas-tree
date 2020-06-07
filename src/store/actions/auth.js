62262657213edd9570df0dcee30fd71b3e8edcd0
darina-m <prosto.chelovek333@gmail.com>
2020-06-08 02:53:33 +0600

    Add firebase URL

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,19 +1,28 @@
 import { AUTH_SUCCESS, AUTH_FAIL, AUTH_START } from "./types";
 import axios from "axios";
 
-export const start = (dispatch) => dispatch({
-  type: AUTH_START
-});
+export const start = (dispatch) =>
+  dispatch({
+    type: AUTH_START,
+  });
 
-export const success = (dispatch, data) => dispatch({
-  type: AUTH_SUCCESS, data
-});
+export const success = (dispatch, data) =>
+  dispatch({
+    type: AUTH_SUCCESS,
+    data,
+  });
 
-export const fail = (dispatch, errors) => dispatch({
-  type: AUTH_FAIL, errors
-});
+export const fail = (dispatch, errors) =>
+  dispatch({
+    type: AUTH_FAIL,
+    errors,
+  });
 
-export const auth = (dispatch, email, password) => axios.post()
-  .then(({ data }) => success(dispatch, data))
-  .catch(error => fail(dispatch, error)); 
-  
+export const auth = (dispatch, email, password) =>
+  axios
+    .post(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCNDNWnsm7XXc3tfClbnz1Mu2AdE99smiI",
+      { email, password }
+    )
+    .then(({ data }) => success(dispatch, data))
+    .catch((error) => fail(dispatch, error));
