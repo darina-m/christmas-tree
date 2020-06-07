571fedae83a7da6fce541a69952f27c56d12af2f
darina-m <prosto.chelovek333@gmail.com>
2020-06-08 03:02:51 +0600

    Add spinner

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -4,28 +4,37 @@ import withAxios from "../../hoc/withAxios/withAxios";
 import classes from "./Auth.module.css";
 import { start, auth } from "../../store/actions/auth";
 import Button from "../UI/Button/Button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default withAxios(() => {
   const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.auth);
 
   const formSubmitted = (event) => {
     start(dispatch);
 
     const data = new FormData(event.target);
-    auth(dispatch, data.get('email'), data.get('password'));
+    auth(dispatch, data.get("email"), data.get("password"));
 
     event.preventDefault();
-  }
+  };
 
-  return (
-    <div className={classes.Auth}>
+  let formOutput = <Spinner />;
+  if (!loading) {
+    formOutput = (
       <form onSubmit={formSubmitted}>
-        <h1>Sign up</h1>
+        <h1>Welcome</h1>
         <input type="email" placeholder="E-mail" name="email" required />
-        <input type="password" placeholder="Password" name="password" required minLength="6" />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+          minLength="6"
+        />
         <Button green>Submit</Button>
       </form>
-    </div>
-  );
-}, axios); 
+    );
+  }
+  return <div className={classes.Auth}>{formOutput}</div>;
+}, axios);
