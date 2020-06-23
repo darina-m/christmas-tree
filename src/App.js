e45bccd79d441471357dda121f4e7cfbe48fbb7f
darina-m <prosto.chelovek333@gmail.com>
2020-06-23 19:55:06 +0600

    Restrict access to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { restore } from "./store/actions/auth";
 import Auth from "./components/Auth/Auth";
 import Layout from "./containers/Layout/Layout.js";
 import ChristmasTree from "./containers/ChristmasTree/ChristmasTree";
@@ -7,39 +9,55 @@ import Checkout from "./components/Checkout/Checkout";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Logout from "./components/Logout/Logout";
-import { restore } from "./store/actions/auth";
-import { useDispatch } from "react-redux";
 
 export default () => {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.token !== null);
   useEffect(() => {
     restore(dispatch);
   }, [dispatch]);
 
+  let routesOutput = (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/builder" />
+      </Route>
+      <Route path="/builder">
+        <ChristmasTree />
+      </Route>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (isAuthenticated) {
+    routesOutput = (
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/builder" />
+        </Route>
+        <Route path="/builder">
+          <ChristmasTree />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+      </Switch>
+    );
+  }
   return (
     <div className="App">
-      <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/builder" />
-          </Route>
-          <Route path="/builder">
-            <ChristmasTree />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>{" "}
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Route path="/logout">
-            <Logout />
-          </Route>
-        </Switch>
-      </Layout>
+      <Layout>{routesOutput}</Layout>
     </div>
   );
 };
